8cd3c74bcdd6f1fd8da9d6683c7e79eabc686eae
graceswerczek <graceswerczek@gmail.com>
2016-06-20 23:43:11 -0400

    add file structure

---
+++ oog.gemspec.rb
@@ -0,0 +1,15 @@
+Gem::Specification.new do |spec|
+	spec.name          = "NAME"
+	spec.version       = "Your Name Here"
+	spec.authors       = "graceswerczek@gmail.com"
+	spec.email         = "Short summary of your project"
+	spec.summary       = "Longer description of your project"
+	spec.description   = ""
+	spec.homepage      = "http://domainforproject.com/"
+	spec.license       = "MIT"
+	spec.files         = 'lib/NAME.rb'
+	spec.executibles   = 'bin/NAME'
+
+	spec.test_files    = 'tests/test_NAME.rb'
+	spec.require_paths = "lib"
+end
