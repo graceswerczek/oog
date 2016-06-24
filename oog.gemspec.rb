18566fc76b1fe1b565596d699a9f387f21c11e52
Grace <graceswerczek@gmail.com>
2016-06-23 22:09:53 -0600

    update .gemspec

--- oog.gemspec.rb
+++ oog.gemspec.rb
@@ -1,10 +1,10 @@
 Gem::Specification.new do |spec|
-	spec.name          = "NAME"
-	spec.version       = "1.0.0"
-	spec.authors       = ["Grace Swerczek"]
-	spec.email         = "graceswerczek@gmail.com"
-	spec.description   = "CLI for googling error messages"
-	spec.license       = "MIT"
-	spec.files         = ['lib/oog.rb, test/test_oog.rb, bin/oog, README.md']
-	spec.test_files    = 'tests/test_NAME.rb'
+	spec.name            = "NAME"
+	spec.version          = "1.0.0"
+	spec.authors         = ["Grace Swerczek"]
+	spec.email             = "graceswerczek@gmail.com"
+	spec.description    = "CLI for googling error messages"
+	spec.license         = "MIT"
+	spec.files              = ['lib/oog.rb, test/test_oog.rb, bin/oog, README.md']
+	spec.test_files      = 'tests/test_NAME.rb'
 end
