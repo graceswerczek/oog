7c96b248f30db77ea5faab8476bed07f21f741fb
graceswerczek <graceswerczek@gmail.com>
2016-06-23 23:59:57 -0400

    update .gemspec

--- oog.gemspec.rb
+++ oog.gemspec.rb
@@ -1,15 +1,10 @@
 Gem::Specification.new do |spec|
 	spec.name          = "NAME"
-	spec.version       = "Your Name Here"
-	spec.authors       = "graceswerczek@gmail.com"
-	spec.email         = "Short summary of your project"
-	spec.summary       = "Longer description of your project"
-	spec.description   = ""
-	spec.homepage      = "http://domainforproject.com/"
+	spec.version       = "1.0.0"
+	spec.authors       = ["Grace Swerczek"]
+	spec.email         = "graceswerczek@gmail.com"
+	spec.description   = "CLI for googling error messages"
 	spec.license       = "MIT"
-	spec.files         = 'lib/NAME.rb'
-	spec.executibles   = 'bin/NAME'
-
+	spec.files         = ['lib/oog.rb, test/test_oog.rb, bin/oog, README.md']
 	spec.test_files    = 'tests/test_NAME.rb'
-	spec.require_paths = "lib"
 end
