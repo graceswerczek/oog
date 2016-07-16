734f0ec4ecfac2053a3c29b313adfe162d48591e
graceswerczek <graceswerczek@gmail.com>
2016-07-15 23:30:03 -0400

    manage extensions

--- oog.gemspec.rb
+++ oog.gemspec.rb
@@ -1,10 +1,11 @@
 Gem::Specification.new do |spec|
 	spec.name          = "NAME"
-	spec.version       = "1.0.0"
+	spec.version       = "0.0.1"
 	spec.authors       = ["Grace Swerczek"]
 	spec.email         = "graceswerczek@gmail.com"
 	spec.description   = "CLI for googling error messages"
 	spec.license       = "MIT"
-	spec.files         = ['lib/oog.rb, test/test_oog.rb, bin/oog, README.md']
+	spec.files         = `git ls-files`.split($/)
 	spec.test_files    = 'tests/test_NAME.rb'
+	spec.date          = '2016-06-01'
 end
