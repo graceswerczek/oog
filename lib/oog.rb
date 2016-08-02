7131fcca30da3b4577499e722e990e7c300c162d
graceswerczek <graceswerczek@gmail.com>
2016-08-01 23:58:26 -0400

    work on lib

--- lib/oog.rb
+++ lib/oog.rb
@@ -1,2 +1,18 @@
 require 'nokogiri'
-require 'socket'
+require 'mechanize'
+require 'optparse'
+
+options = {}
+
+parser = OptionParser.new do |opts|
+  opts.banner = "answers from the internet in your command line"
+  
+  opts.on("-h", "--help", "display help") |h|
+    puts opts
+    exit
+  end
+end.parse!
+
+ARGV.each do |word|
+  words << word.dup
+end
